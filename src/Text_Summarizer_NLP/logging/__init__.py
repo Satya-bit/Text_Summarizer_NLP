a7dfc44b6ff2870f8eb38e71a0aaf7c0dae5d794
SATYA SHAH <70309925+Satya-bit@users.noreply.github.com>
2025-02-27 21:35:01 -0600

    Update file

--- src/Text_Summarizer_NLP/logging/__init__.py
+++ src/Text_Summarizer_NLP/logging/__init__.py
@@ -1,14 +1,42 @@
+# import os
+# import sys
+# import logging
+
+# log_dir="logs"
+# logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+# log_filepath = os.path.join(log_dir,"continuos_logs.log")
+
+# os.makedirs(log_dir,exist_ok=True)# If that directory already exists not going to create it again
+
+
+# # Configuring the logging settings
+# logging.basicConfig(
+#     level=logging.INFO,  # Sets the logging level to INFO, meaning it will log INFO and higher-level messages
+#     format=logging_str,  # Defines the log message format 
+    
+#     handlers=[
+#         logging.FileHandler(log_filepath),  # Logs messages to a file specified by log_filepath
+#         logging.StreamHandler(sys.stdout)  # Also prints log messages to the console (stdout)
+#     ]
+# )
+
+# # Creating a logger instance named "summarizerlogger"
+# logger = logging.getLogger("summarizerlogger")
+
 import os
 import sys
 import logging
 
-log_dir="logs"
+# Define the directory where logs will be stored
+log_dir = os.path.join("/tmp", "logs")  # Change log directory to /tmp to avoid permission issues
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-log_filepath = os.path.join(log_dir,"continuos_logs.log")
+# Define the log file path inside the log directory
+log_filepath = os.path.join(log_dir, "continuos_logs.log")
 
-os.makedirs(log_dir,exist_ok=True)# If that directory already exists not going to create it again
-
+# Create the log directory if it does not exist
+os.makedirs(log_dir, exist_ok=True)  # If that directory already exists, it will not create it again
 
 # Configuring the logging settings
 logging.basicConfig(
@@ -23,3 +51,5 @@ logging.basicConfig(
 
 # Creating a logger instance named "summarizerlogger"
 logger = logging.getLogger("summarizerlogger")
+
+###
